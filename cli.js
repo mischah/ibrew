db2a7a0be8bd410d11818f0d2fd75fd0b29992f5
Michael Kühnel <m.kuehnel@micromata.de>
2019-02-25 08:39:43 +0100

    chore: Setup and apply editorconfig

--- cli.js
+++ cli.js
@@ -8,101 +8,101 @@ const ora = require('ora');
 const getStream = require('get-stream');
 
 const cli = meow(
-	`
-	Usage
-	  $ ibrew <searchterm>
+  `
+  Usage
+    $ ibrew <searchterm>
 
-	Options
-	  --help, -h      Show help
-	  --version, -v   Print version number
+  Options
+    --help, -h      Show help
+    --version, -v   Print version number
 
-	Example
-	  $ ibrew say
+  Example
+    $ ibrew say
 `,
-	{
-		alias: {
-			h: 'help',
-			v: 'version'
-		}
-	}
+  {
+    alias: {
+      h: 'help',
+      v: 'version'
+    }
+  }
 );
 
 const [searchTerm] = cli.input;
 const spinner = ora('Searching for packages');
 
 if (cli.input.length === 0 && cli.flags.v === true) {
-	cli.showVersion();
+  cli.showVersion();
 }
 
 if (cli.input.length === 0 && cli.flags.h === true) {
-	cli.showHelp(0);
+  cli.showHelp(0);
 }
 
 if (cli.input.length > 1) {
-	console.log(
-		`\n${logSymbols.error} Invalid input. Please check the help below:`
-	);
-	cli.showHelp();
+  console.log(
+    `\n${logSymbols.error} Invalid input. Please check the help below:`
+  );
+  cli.showHelp();
 }
 
 // Exits program execution on ESC
 process.stdin.on('keypress', (ch, key) => {
-	if (key && key.name === 'escape') {
-		process.exit(0);
-	}
+  if (key && key.name === 'escape') {
+    process.exit(0);
+  }
 });
 
 (async () => {
-	spinner.start();
-	const { stdout: result } = await execa('brew', ['search', searchTerm]);
-	const choices = result.split('\n').map(choice => {
-		if (choice.includes('==>')) {
-			return {
-				name: choice.substring(4),
-				disabled: choice.includes('Formulae')
-					? 'Command line packages'
-					: 'GUI macOS applications'
-			};
-		}
-		if (choice === '') {
-			return new inquirer.Separator();
-		}
-		return choice;
-	});
+  spinner.start();
+  const { stdout: result } = await execa('brew', ['search', searchTerm]);
+  const choices = result.split('\n').map(choice => {
+    if (choice.includes('==>')) {
+      return {
+        name: choice.substring(4),
+        disabled: choice.includes('Formulae')
+          ? 'Command line packages'
+          : 'GUI macOS applications'
+      };
+    }
+    if (choice === '') {
+      return new inquirer.Separator();
+    }
+    return choice;
+  });
 
-	choices.push(new inquirer.Separator());
+  choices.push(new inquirer.Separator());
 
-	if (result) {
-		spinner.succeed(
-			`Found ${
-				choices.filter(choice => typeof choice === 'string').length
-			} packages\n`
-		);
-	} else {
-		spinner.info(`No formula or cask found for \`${searchTerm}\`.`);
-		process.exit(0);
-	}
+  if (result) {
+    spinner.succeed(
+      `Found ${
+        choices.filter(choice => typeof choice === 'string').length
+      } packages\n`
+    );
+  } else {
+    spinner.info(`No formula or cask found for \`${searchTerm}\`.`);
+    process.exit(0);
+  }
 
-	const selected = await inquirer.prompt([
-		{
-			type: 'list',
-			name: 'package',
-			message: 'Which package you would like to install?',
-			pageSize: 10,
-			choices
-		}
-	]);
+  const selected = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'package',
+      message: 'Which package you would like to install?',
+      pageSize: 10,
+      choices
+    }
+  ]);
 
-	spinner.start(`Installing \`${selected.package}\``);
-	const stream = execa('brew', ['install', selected.package]);
+  spinner.start(`Installing \`${selected.package}\``);
+  const stream = execa('brew', ['install', selected.package]);
 
-	stream.stdout.on('data', data => {
-		spinner.stop();
-		console.log(data.toString().trim());
-	});
+  stream.stdout.on('data', data => {
+    spinner.stop();
+    console.log(data.toString().trim());
+  });
 
-	getStream(stream.stderr).then(stderr => {
-		spinner.stop();
-		console.log(stderr && `${logSymbols.warning} ${stderr.trim()}`);
-	});
+  getStream(stream.stderr).then(stderr => {
+    spinner.stop();
+    console.log(stderr && `${logSymbols.warning} ${stderr.trim()}`);
+  });
 })();
