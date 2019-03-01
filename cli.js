f5b1cdc80410708b31f8819025999083c8f20d4e
Michael Kühnel <m.kuehnel@micromata.de>
2019-03-01 01:27:29 +0100

    refactor: DRY

--- cli.js
+++ cli.js
@@ -4,13 +4,10 @@ const meow = require('meow');
 const logSymbols = require('log-symbols');
 const inquirer = require('inquirer');
 const ora = require('ora');
-const getStream = require('get-stream');
 
 const search = require('./lib/search');
-const install = require('./lib/install');
-const installed = require('./lib/installed');
-const upgrade = require('./lib/upgrade');
-const remove = require('./lib/remove');
+const { selectInstalledPackage } = require('./lib/installed');
+const { executeCommand } = require('./lib/execute-command');
 
 const cli = meow(
   `
@@ -118,28 +115,18 @@ process.stdin.on('keypress', (ch, key) => {
 // List installed packages to upgrade
 if (cli.flags.upgrade === true) {
   (async () => {
-    const selected = await installed.selectInstalledPackage({
+    const { packages } = await selectInstalledPackage({
       message: 'Which packages you would like to upgrade?',
       validationError: 'Please select at least one package to upgrade.',
       pageSize: Number(cli.flags.size) || defaultPageSize.installed,
       spinner
     });
 
-    spinner.start(`Upgrading selected packages(s)`);
-    const stream = upgrade.getOutput(selected.packages);
-    stream.stdout.on('data', data => {
-      spinner.stop();
-      console.log(data.toString().trim());
-    });
-    stream.stderr.on('data', data => {
-      spinner.stop();
-      console.log(
-        data.toString() &&
-          data
-            .toString()
-            .trim()
-            .replace(/Error/gm, `${logSymbols.error} Error`)
-      );
+    executeCommand({
+      command: 'upgrade',
+      packages,
+      spinner,
+      loadingMessage: 'Upgrading selected packages(s)'
     });
   })();
 }
@@ -147,28 +134,18 @@ if (cli.flags.upgrade === true) {
 // List installed packages to remove
 if (cli.flags.remove === true) {
   (async () => {
-    const selected = await installed.selectInstalledPackage({
+    const { packages } = await selectInstalledPackage({
       message: 'Which packages you would like to uninstall?',
       validationError: 'Please select at least one package to uninstall.',
       pageSize: Number(cli.flags.size) || defaultPageSize.installed,
       spinner
     });
 
-    spinner.start(`Uninstalling selected packages(s)`);
-    const stream = remove.getOutput(selected.packages);
-    stream.stdout.on('data', data => {
-      spinner.stop();
-      console.log(data.toString().trim());
-    });
-    stream.stderr.on('data', data => {
-      spinner.stop();
-      console.log(
-        data.toString() &&
-          data
-            .toString()
-            .trim()
-            .replace(/Error/gm, `${logSymbols.error} Error`)
-      );
+    executeCommand({
+      command: 'remove',
+      packages,
+      spinner,
+      loadingMessage: 'Uninstalling selected packages(s)'
     });
   })();
 }
@@ -193,26 +170,18 @@ if (cli.flags.upgrade === false && cli.flags.remove === false) {
     const selected = await inquirer.prompt([
       {
         type: 'list',
-        name: 'packages',
+        name: 'package',
         message: 'Which package you would like to install?',
         pageSize: Number(cli.flags.size) || defaultPageSize.searchResult,
         choices
       }
     ]);
 
-    console.log('selection =', selected.packages);
-
-    spinner.start(`Installing \`${selected.package}\``);
-    const stream = install.getOutput(selected.package);
-
-    stream.stdout.on('data', data => {
-      spinner.stop();
-      console.log(data.toString().trim());
-    });
-
-    getStream(stream.stderr).then(stderr => {
-      spinner.stop();
-      console.log(stderr && `${logSymbols.warning} ${stderr.trim()}`);
+    executeCommand({
+      command: 'install',
+      packages: [selected.package],
+      spinner,
+      loadingMessage: `Installing \`${selected.package}\``
     });
   })();
 }
